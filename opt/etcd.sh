2dc74d0583f21d34ce1a3fcf6c5b6632c976841e
Chuanjian Wang <me@ckeyer.com>
2018-04-27 19:13:23 +0800

    up

--- opt/etcd.sh
+++ opt/etcd.sh
@@ -7,7 +7,7 @@ docker run  -d \
  --name $APP \
  -p 2379:2379 \
  -v /data/etcdv3:/data \
-quay.io/coreos/etcd:v3.3 /usr/local/bin/etcd \
+registry.cn-beijing.aliyuncs.com/wa/etcd:v3.3 /usr/local/bin/etcd \
  --name=eeeee \
  --data-dir=/data \
  --listen-client-urls=http://0.0.0.0:2379 \
