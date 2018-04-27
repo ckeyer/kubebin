2ba104545d745648a74c3432b59dc91205905b87
Chuanjian Wang <me@ckeyer.com>
2018-04-27 19:27:50 +0800

    use dif port'

--- opt/etcd.sh
+++ opt/etcd.sh
@@ -5,10 +5,10 @@ set -ex;
 docker rm $APP || echo "not found $APP";
 docker run  -d \
  --name $APP \
- -p 2379:2379 \
+ -p 22379:22379 \
  -v /data/etcdv3:/data \
 registry.cn-beijing.aliyuncs.com/wa/etcd:v3.3 /usr/local/bin/etcd \
  --name=eeeee \
  --data-dir=/data \
- --listen-client-urls=http://0.0.0.0:2379 \
- --advertise-client-urls=http://0.0.0.0:2379
+ --listen-client-urls=http://0.0.0.0:22379 \
+ --advertise-client-urls=http://0.0.0.0:22379
